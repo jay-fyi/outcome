0e3cc4ca3b3bf402ef6eb6f94bd7dbcee53fd8f3
Jason Yu <jyu@Jasons-MBP.hsd1.ca.comcast.net>
2020-03-02 20:01:48 -0800

    week1 Q1

---
+++ src/main/java/outco/outcome/question/model/TreeNode.java
@@ -0,0 +1,17 @@
+package outco.outcome.question.model;
+
+public class TreeNode<T> {
+
+    public T val;
+    public TreeNode<T> left;
+    public TreeNode<T> right;
+
+    public TreeNode(T x) {
+        val = x;
+    }
+
+    public TreeNode() {
+    }
+}
+
+
